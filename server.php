3fa57eb85659776ab18d994e03b10c1824cc9d2f
tuevms <tuevms@gmail.com>
2016-07-01 19:53:59 +0200

    netbeans2

--- server.php
+++ server.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Laravel - A PHP Framework For Web Artisans
+ * Laravel1 - A PHP Framework For Web Artisans
  *
  * @package  Laravel
  * @author   Taylor Otwell <taylorotwell@gmail.com>
